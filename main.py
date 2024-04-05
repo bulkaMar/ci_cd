6f89078e1cb14e12d5f3202dbd62b7916bf6e196
Marina Bulakh <bulahmarina@knu.ua>
2024-04-05 13:56:04 +0300

    add sort area function

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 def read_population_data(file_name):
+    """
+    Приймає рядок file_name, який представляє ім'я файлу із даними про популяцію.
+    Повертає список кортежів, де кожен кортеж містить інформацію про країну у форматі (назва, площа, населення).
+    """
     population_data = []
     with open(file_name, 'r') as file:
         for line in file:
@@ -6,4 +10,11 @@ def read_population_data(file_name):
             population_data.append((country, float(area), int(population)))
     return population_data
 
+
+def sort_by_area(population_data):
+    """
+    Приймає список кортежів population_data, де кожен кортеж містить інформацію про країну у форматі (назва, площа, населення).
+    Повертає список кортежів, відсортований за зростанням площі країн.
+    """
+    return sorted(population_data, key=lambda x: x[1])
 
